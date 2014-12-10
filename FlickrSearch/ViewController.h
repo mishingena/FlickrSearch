596b983a25fa69ffc3e0fe7ae0f4ea671bdcc993
Гена <gena@iMac-Rafael1.local>
2014-12-10 16:33:19 +0300

    Initial Commit

---
+++ FlickrSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlickrSearch
+//
+//  Created by Гена on 10.12.14.
+//  Copyright (c) 2014 fix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FlickrSearch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlickrSearch
+//
+//  Created by Гена on 10.12.14.
+//  Copyright (c) 2014 fix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
